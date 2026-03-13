6c7f21da61a664a9c13e2be24971a84578e5ab17
Leah De Laurell <52628574+leahdelaurell@users.noreply.github.com>
2026-03-13 11:14:44 -0600

    remove old code

--- assets/js/a7456010.9f0e2c1c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkpersonal_webbie=globalThis.webpackChunkpersonal_webbie||[]).push([[235],{8552(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
